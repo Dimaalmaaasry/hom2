1f76bdafa9ee1319c692cddbaf3a2766f12ed310
Dimaalmaaasry <moeen.wm.8@gmail.com>
2021-06-24 21:53:34 +0300

    hom

---
+++ src/compontnet/intro.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './intro.css';
+function Intro (){
+    return (
+        <div >
+            <h1 className = 'dima'>Hello world!!</h1>
+            <img src="https://scontent.fgza11-1.fna.fbcdn.net/v/t1.15752-9/110124251_313326606471090_938678816944368119_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=ae9488&_nc_ohc=YU41cYG2Zd4AX8Uz2Iu&_nc_ht=scontent.fgza11-1.fna&oh=f2f77784a5bfc2ee6e331d0ba9cb47a1&oe=60D98F53" alt=" " />
+            <div className ="text">
+             <h6>I'm dima almassry , 17 years old</h6> 
+             <h6>fav sport : swimming & basketball</h6>
+             <h6>fav moives unitl now : experience old never get</h6>
+             <h6>fav book until now: fourty for ahmed alshqiri</h6>
+             <h6>Palestine-Gaza</h6>
+             <a href="https://www.facebook.com/dima.almasry.1/">click here to go to myface acount</a>
+
+            
+            </div>
+        </div>
+ 
+    )
+}
+export default Intro ;
+//export const Intro =()=> <h1>Hello world!</h1>
